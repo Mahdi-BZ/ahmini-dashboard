15cc9037981d44836a110aacbfde53f130e20abd
Louay ElOudi <louayinfo07@gmail.com>
2021-12-30 01:48:15 +0100

    typo fix

--- src/app/pages/registeredWoman/SingleRegisteredWomanPage.tsx
+++ src/app/pages/registeredWoman/SingleRegisteredWomanPage.tsx
@@ -71,8 +71,8 @@ const SingleRegisteredWomanPage: React.FC<Props> = ({className}) => {
       {/* begin::Header */}
       <div className='card-header border-0 pt-5'>
         <h3 className='card-title align-items-start flex-column'>
-          <span className='card-label fw-bolder fs-3 mb-1'>Woman </span>
-          <span className='text-muted mt-1 fw-bold fs-7'>Informations d'ambassadeur détailées</span>
+          <span className='card-label fw-bolder fs-3 mb-1'>Femme </span>
+          <span className='text-muted mt-1 fw-bold fs-7'>Informations de la femme détailées</span>
         </h3>
         <div className='card-toolbar w-25'>
           {particularsApiData && (
